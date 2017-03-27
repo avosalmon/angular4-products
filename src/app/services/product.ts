dd904e173f5d38b9ccb05a1e05932cf184bd992b
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2017-03-27 17:49:06 +0900

    add properties to Product class

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -3,7 +3,22 @@ export class Product {
   product_name: string;
   model_number: string;
   display_state: string;
+  state: string;
   public_stock: number;
   real_stock: number;
   image_uri: string;
+  cost: number;
+  markup: number;
+  price: number;
+  discount_price: number;
+  members_price: number;
+  unit: string;
+  max_purchase_count: number;
+  wrappable: number;
+  tag: string;
+  jan_code: string;
+  search_engine_title: string;
+  search_engine_description: string;
+  facebook_title: string;
+
 }
