53b716d52b92ced3584cc7097188b57246dd90a5
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2017-03-28 09:52:49 +0900

    implement updateProduct method

--- src/app/services/products.api.ts
+++ src/app/services/products.api.ts
@@ -12,7 +12,7 @@ export class ProductsApi {
 
   private headers = new Headers({
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer '
+    'Authorization': 'Bearer token'
   });
 
   constructor(private http: Http) { }
@@ -29,6 +29,14 @@ export class ProductsApi {
     return this.http.get(url, { headers: this.headers })
       .toPromise()
       .then(response => response.json().data.product as Product)
+      .catch(this.handleError);
+  }
+
+  updateProduct(product: Product): Promise<Product> {
+    const url = `${this.endpoint}/${product.id}`;
+    return this.http.put(url, JSON.stringify(product), { headers: this.headers })
+      .toPromise()
+      .then(() => product)
       .catch(this.handleError);
   }
 
